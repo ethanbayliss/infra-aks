23bd428745ff87aa8636aa5c7fa9c3f4b22443b9
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-06 13:16:04 +0000

    #patch sensitive outputs

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,11 @@ output "cluster_private_fqdn" {
 }
 
 output "cluster_kube_config" {
-  value = azurerm_kubernetes_cluster.this.kube_config
+  value     = azurerm_kubernetes_cluster.this.kube_config
+  sensitive = true
 }
 
 output "cluster_kube_config_raw" {
-  value = azurerm_kubernetes_cluster.this.kube_config_raw
+  value     = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive = true
 }
