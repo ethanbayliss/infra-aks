b5e8106a0944e676602bba9ccadf11bd950ac798
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-06 12:13:59 +0000

    #minor update azure provider

--- provider.tf
+++ provider.tf
@@ -18,13 +18,11 @@ variable "tags" {
   }
 }
 
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.59"
+      version = "~>3.63"
     }
   }
   cloud {
