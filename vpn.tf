283c026518ecf105a944ec56a16158e64e99ef1e
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-07 12:38:30 +0000

    #patch security group on public interface

--- vpn.tf
+++ vpn.tf
@@ -1,7 +1,8 @@
 locals {
   instance_name = "openvpn"
   # ssh-ed25519 not supported
-  ssh_rsa_key   = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWNfRM+3wcvyCrKlFucwcs7ssij0mCauloprMu5evcVu+gyiG9kYggl9uq4d59O2q8ThJ1XBayhd//IGSLGO/Q8QwWjU0RM6ZLfM1IQmDkkYDTPvanIDJtCr4RqBu12qkOn+swIN1vy90h6jAHS6prDqkPFwuwbTPbzdyShNgmOlH1D6Q8Hlf8B1x0wUDjARNAork3D4V2OCMndQwYR7tEGdDwIjuH1qV+PZPhZ0Wcs7uNF4737FISY2gkgAYiw/n0ZaU9K3qm3bmDoK9/G7qoOOUeBQ/rJ4g56ytITxT8uvmQrIiIQyXcmw+7AR+MGSVWYWvg5B2MLQYX5e1aQbxCCnZuYQO874ajq051YhhvV6l3y0jWSXFdi21guKCFFf4C0W2UF2OJM2F1Qhuko7vUMjX4N48YD3XRXb190hmod9fH0/Kts+nmJX3YVgXuEo9AHdAAuFlyRZpAOzBneJ1nf4PSzk1ZO524P3LQ1NLfuVxafj0JPZGy8V1+NdSzq3U= ebayliss@DESKTOP-4NGKHUI"
+  ssh_rsa_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWNfRM+3wcvyCrKlFucwcs7ssij0mCauloprMu5evcVu+gyiG9kYggl9uq4d59O2q8ThJ1XBayhd//IGSLGO/Q8QwWjU0RM6ZLfM1IQmDkkYDTPvanIDJtCr4RqBu12qkOn+swIN1vy90h6jAHS6prDqkPFwuwbTPbzdyShNgmOlH1D6Q8Hlf8B1x0wUDjARNAork3D4V2OCMndQwYR7tEGdDwIjuH1qV+PZPhZ0Wcs7uNF4737FISY2gkgAYiw/n0ZaU9K3qm3bmDoK9/G7qoOOUeBQ/rJ4g56ytITxT8uvmQrIiIQyXcmw+7AR+MGSVWYWvg5B2MLQYX5e1aQbxCCnZuYQO874ajq051YhhvV6l3y0jWSXFdi21guKCFFf4C0W2UF2OJM2F1Qhuko7vUMjX4N48YD3XRXb190hmod9fH0/Kts+nmJX3YVgXuEo9AHdAAuFlyRZpAOzBneJ1nf4PSzk1ZO524P3LQ1NLfuVxafj0JPZGy8V1+NdSzq3U= ebayliss@DESKTOP-4NGKHUI"
+  admin_ip    = "159.196.85.197/32"
 }
 
 resource "azurerm_virtual_machine" "vpn" {
@@ -60,6 +61,27 @@ resource "azurerm_public_ip" "vpn" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "vpn_public_sg" {
+  name                = "${terraform.workspace}-vpn-public-sg"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  security_rule {
+    name                   = "ssh"
+    priority               = 100
+    direction              = "Inbound"
+    access                 = "Allow"
+    protocol               = "Tcp"
+    destination_port_range = "22"
+    source_address_prefix  = local.admin_ip
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "vpn_public_sg" {
+  network_interface_id      = azurerm_network_interface.vpn_public.id
+  network_security_group_id = azurerm_network_security_group.vpn_public_sg.id
 }
 
 resource "azurerm_network_interface" "vpn_private" {
