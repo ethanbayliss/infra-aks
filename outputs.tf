21a1ccd1f5b83b74dfa94ec82471decc444e5f4f
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-07 10:05:07 +0000

    #patch remove output too

--- outputs.tf
+++ outputs.tf
@@ -15,7 +15,3 @@ output "cluster_kube_config_raw" {
   value     = azurerm_kubernetes_cluster.this.kube_config_raw
   sensitive = true
 }
-
-output "cluster_dns_zone_id" {
-  value = azurerm_private_dns_zone.cluster.id
-}
