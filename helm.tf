46fe07772897e39140c21f9ca0c47cc9bc578e7b
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-13 13:36:52 +0000

    #patch helm chart name no prefix

--- helm.tf
+++ helm.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "vanilla" {
   name       = "vanilla"
   repository = "https://itzg.github.io/minecraft-server-charts/"
-  chart      = "itzg/minecraft"
+  chart      = "minecraft"
 
   values = [
     "${file("${path.module}/src/helm/minecraft_vanilla.yml")}"
