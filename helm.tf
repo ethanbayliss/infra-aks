bdfaafb04c99aae877f1a69421bf58a7e74fcb13
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-13 13:32:20 +0000

    #patch forgot chart name

--- helm.tf
+++ helm.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "vanilla" {
   name       = "vanilla"
   repository = "https://itzg.github.io/minecraft-server-charts/"
+  chart      = "itzg/minecraft"
 
   values = [
     "${file("${path.module}/src/helm/minecraft_vanilla.yml")}"
