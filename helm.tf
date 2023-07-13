ad09b15ea248859b75c15b79f59fdd228cde81df
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-07-13 14:08:36 +0000

    #patch remove helm testing

--- helm.tf
+++ helm.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "vanilla" {
-  name       = "vanilla"
-  repository = "https://itzg.github.io/minecraft-server-charts/"
-  chart      = "minecraft"
+# resource "helm_release" "vanilla" {
+#   name       = "vanilla"
+#   repository = "https://itzg.github.io/minecraft-server-charts/"
+#   chart      = "minecraft"
 
-  values = [
-    "${file("${path.module}/src/helm/minecraft_vanilla.yml")}"
-  ]
-}
+#   values = [
+#     "${file("${path.module}/src/helm/minecraft_vanilla.yml")}"
+#   ]
+# }
