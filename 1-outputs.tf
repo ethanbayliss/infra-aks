b178dec57c1ce6c331f24f631da8c200dadb53ff
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2023-08-08 14:23:27 +0000

    #patch output dns zone ns records

--- 1-outputs.tf
+++ 1-outputs.tf
@@ -1,4 +1,12 @@
 output "cluster_endpoint" {
   description = "This cluster endpoint should be accessible over the VPN"
-  value = azurerm_dns_a_record.kubernetes_api.fqdn
+  value       = azurerm_dns_a_record.kubernetes_api.fqdn
+}
+
+output "dns_config" {
+  description = "Use these NS records to propagate DNS"
+  value       = {
+    domain     = local.public_dns_zone
+    ns_records = azurerm_dns_zone.public_zone.name_servers
+  }
 }
--- 2-dns.tf
+++ 2-dns.tf
@@ -1,5 +1,9 @@
+locals {
+  public_dns_zone = "${var.cluster_name}.${var.public_dns_zone}"
+}
+
 resource "azurerm_dns_zone" "public_zone" {
-  name                = "${var.cluster_name}.${var.public_dns_zone}"
+  name                = local.public_dns_zone
   resource_group_name = azurerm_resource_group.this.name
 
   tags = var.tags
